88bef1499a357b52198bd38b6c07ba914be8411b
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-02-23 15:24:12 +0100

    Finished the first part tut

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+
+import Post from "./Post.js/Post"
+import useStyles from "./styles";
+
+const Posts = () => {
+    const posts = useSelector((state) => state.posts)
+    const classes = useStyles();
+
+    console.log(posts)
+    return (
+        <>
+        <h1>Posts</h1>
+        <Post/>
+        <Post/>
+        </>
+    )
+}
+
+export default Posts
